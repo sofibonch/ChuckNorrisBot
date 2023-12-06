1fa10683ce42940bb21a7ac876dbb0a8b3e01b6d
sofi <sofibonch@gmail.com>
2023-12-06 15:20:19 +0200

    Update chucknorris_bot.js
    added handling wrong commands and requests

--- chucknorris_bot.js
+++ chucknorris_bot.js
@@ -2,10 +2,15 @@ const { Telegraf } = require('telegraf');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+// Telegram token
 const bot = new Telegraf('6865982965:AAFG9O2QH5oVvD9D-NzaeYEIprGzKsqloRM');
 
 // Store user settings (language) in memory
 const userSettings = {};
+
+// Set default language to English
+const defaultLanguage = 'en';
+userSettings.defaultLanguage = defaultLanguage;
 
 // Azure Translation API key
 const translationApiKey = 'fb89e91d643942e498f67e3bd1daba87';
@@ -17,6 +22,9 @@ const translationApiEndpoint = 'https://api.cognitive.microsofttranslator.com/tr
 // Language mapping object (populated dynamically)
 let languageMapping = {};
 
+// Define a placeholder for text that should not be translated
+const NO_TRANSLATE_PLACEHOLDER = '__NO_TRANSLATE__';
+
 // Function to fetch supported languages from Azure Translation API
 const fetchSupportedLanguages = async () => {
   try {
@@ -26,15 +34,13 @@ const fetchSupportedLanguages = async () => {
       },
     });
 
-    // Populate the language mapping dynamically
     const supportedLanguages = Object.keys(response.data.translation);
     languageMapping = supportedLanguages.reduce((map, language) => {
       map[response.data.translation[language].name.toLowerCase()] = language;
       return map;
     }, {});
-
-    console.log('Supported languages:', languageMapping);
   } catch (error) {
+    ctx.reply('Sorry, there was a problem getting the language options. Please try again.');
     console.error('Error fetching supported languages:', error.message);
   }
 };
@@ -44,6 +50,7 @@ const getLanguageCode = (languageName) => {
   return languageMapping[languageName.toLowerCase()] || languageName;
 };
 
+// Function for getting list of Chuck Norris joke
 const scrapeChuckNorrisJokes = async () => {
   try {
     const response = await axios.get('https://parade.com/968666/parade/chuck-norris-jokes/', {
@@ -56,7 +63,6 @@ const scrapeChuckNorrisJokes = async () => {
     });
     const $ = cheerio.load(response.data);
 
-    // Extract jokes from the page
     const jokes = [];
     $('ol li').each((index, element) => {
       jokes.push($(element).text().trim());
@@ -64,6 +70,9 @@ const scrapeChuckNorrisJokes = async () => {
 
     return jokes;
   } catch (error) {
+    const errormessage = 'Sorry, there was a problem getting your Chuck Norris jokes. Please try again.';
+    const translatederrormessage = await translateText(errormessage, targetLanguage);
+    ctx.reply(translatederrormessage);
     console.error('Error fetching Chuck Norris jokes:', error.message);
     return [];
   }
@@ -89,73 +98,128 @@ const translateText = async (text, targetLanguage) => {
         },
       }
     );
-
-    console.log('Translation response:', translationResponse.data);
-
     return translationResponse.data[0].translations[0].text;
   } catch (error) {
+    ctx.reply('Sorry, there was a problem the translation. Please ensure your language setting is configured correctly.');
     console.error('Error translating text:', error.message);
     return text;
   }
 };
 
+// Function to handle the user request to set language
 bot.hears(/^set language (.+)$/i, async (ctx) => {
-  // Extract the target language from the user's message
   const targetLanguageName = ctx.match[1];
 
-  // Fetch supported languages if not fetched yet
   if (Object.keys(languageMapping).length === 0) {
     await fetchSupportedLanguages();
   }
 
-  // Get the language code from the full language name
   const targetLanguage = getLanguageCode(targetLanguageName);
 
-  // Translate the response message into the user's preferred language
   const translatedResponse = await translateText('No problem!', targetLanguage);
 
-  // Store the user's preferred language in memory
   userSettings[ctx.message.from.id] = targetLanguage;
 
-  // Respond to the user in their preferred language
   ctx.reply(translatedResponse);
 });
 
+// Function to handle the user request for a joke
 bot.hears(/^\d+$/, async (ctx) => {
-  // Check if the user has set a language preference
   const userId = ctx.message.from.id;
   const targetLanguage = userSettings[userId];
 
-  // Fetch Chuck Norris jokes
   const jokes = await scrapeChuckNorrisJokes();
 
-  // Get the index provided by the user
   const index = parseInt(ctx.message.text);
-  console.log("number of a joke: ", index)
-  console.log("the jokes:", jokes)
-  // Check if the index is valid
-  //if () {
-  //  ctx.reply('did not got accecss to jokes');
-  //  return;
-  // }
 
-  if (isNaN(index) || index < 1 || index > jokes.length) {
-    ctx.reply('Please provide a valid index between 1 and 101.');
+  if (jokes.length === 0) {
+    const errormessage = 'Sorry, we encountered an issue while trying to find your requested joke. Please try again, ensuring that your number is between 1 and 101.'
+    const translatederrormessage = await translateText(errormessage, targetLanguage);
+    ctx.reply(translatederrormessage);
     return;
   }
 
-  // Get the selected joke
+  if (isNaN(index) || index < 1 || index > jokes.length) {
+    const errormessage = 'Please provide a valid index between 1 and 101.';
+    const translatederrormessage = await translateText(errormessage, targetLanguage);
+    ctx.reply(translatederrormessage);
+    return;
+  }
+
   const selectedJoke = jokes[index - 1];
 
-  // Translate the joke using the Azure Translation API
   const translatedJoke = await translateText(selectedJoke, targetLanguage);
 
-  // Reply to the user with the translated joke
-  ctx.reply(translatedJoke);
+  const jokeWithNumber = `${index}. ${translatedJoke}`;
+  ctx.reply(jokeWithNumber);
+});
+
+// Function to handle the "/start" command
+bot.start(async (ctx) => {
+  const userId = ctx.message.from.id;
+  const targetLanguage = userSettings[userId] || userSettings.defaultLanguage || 'en';
+
+  const startMessage = `
+  Welcome to the Chuck Norris Jokes Bot!
+
+  This bot will provide you with 101 Chuck Norris jokes in your preferred language!
+  To get your laughs, this bot can do the following:
+
+  - Set your preferred language using the command
+   "${NO_TRANSLATE_PLACEHOLDER}set ${NO_TRANSLATE_PLACEHOLDER}language ${NO_TRANSLATE_PLACEHOLDER}<language>"
+  - Retrieve Chuck Norris jokes by providing a number (e.g., 5"), number must be between 1 and 101
+  - Use command "/help" to get a list of available actions
+  - Use command "/start" to see this message again
+
+  Example usage:
+  - Set language to hebrew:
+   "${NO_TRANSLATE_PLACEHOLDER}set ${NO_TRANSLATE_PLACEHOLDER}language ${NO_TRANSLATE_PLACEHOLDER}hebrew"
+  - Get Chuck Norris joke #5 in Hebrew
+
+  Enjoy the jokes!
+  `;
+
+  const translatedIntroMessage = await translateText(startMessage, targetLanguage);
+
+  const finalIntroMessage = translatedIntroMessage.replace(new RegExp(NO_TRANSLATE_PLACEHOLDER, 'g'), '');
+
+  ctx.reply(finalIntroMessage);
+});
+
+// Function to handle the "/help" command
+bot.help(async (ctx) => {
+  const userId = ctx.message.from.id;
+  const targetLanguage = userSettings[userId] || userSettings.defaultLanguage || 'en';
+
+  const helpMessage = `
+  This bot can do the following:
+  - Set your preferred language using the command
+  "${NO_TRANSLATE_PLACEHOLDER}set ${NO_TRANSLATE_PLACEHOLDER}language ${NO_TRANSLATE_PLACEHOLDER}<language>"
+  - Retrieve Chuck Norris jokes by providing a number (e.g., "5"), number must be between 1 and 101
+
+  Example usage:
+  - Set language to Hebrew:
+   "${NO_TRANSLATE_PLACEHOLDER}set ${NO_TRANSLATE_PLACEHOLDER}language ${NO_TRANSLATE_PLACEHOLDER}hebrew"
+  - Get Chuck Norris joke #5 in Hebrew
+  `;
+
+  const translatedHelpMessage = await translateText(helpMessage, targetLanguage);
+
+  const finalHelpMessage = translatedHelpMessage.replace(new RegExp(NO_TRANSLATE_PLACEHOLDER, 'g'), '');
+
+  ctx.reply(finalHelpMessage);
+});
+
+//Function to handle invalid commands and texts
+bot.on('text', async (ctx) => {
+  const userId = ctx.message.from.id;
+  const targetLanguage = userSettings[userId] || userSettings.defaultLanguage || 'en';
+
+  const errorMessage = 'Sorry, I didn\'t understand that command. Please use a valid command or type "/help" for assistance.';
+  const translatedErrorMessage = await translateText(errorMessage, targetLanguage);
+
+  ctx.reply(translatedErrorMessage);
 });
 
 // Start the bot
 bot.launch();
-
-// Starting message
-console.log('ChuckNorris Bot is now active!');
